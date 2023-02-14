79b442138a800e70855dfa8d9eb14c78160155db
RaYul18 <prettykiki92@gmail.com>
2023-02-14 23:14:39 +0900

    It works!

---
+++ homework/src/test/java/com/codestates/github_homework/GithubHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.github_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
